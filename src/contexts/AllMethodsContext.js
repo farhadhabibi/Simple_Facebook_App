e82e2f42bec6cbc85a4d5a86f164df0a03277113
farhad-habibi <farhadmalekzada@gmail.com>
2022-12-10 18:38:25 +0500

    update the AllMethodsContext file based on reducer

--- src/contexts/AllMethodsContext.js
+++ src/contexts/AllMethodsContext.js
@@ -1,14 +1,18 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useReducer } from 'react';
 
-import useAllMethodsState from '../hooks/useAllMethodsState';
+// import useAllMethodsState from '../hooks/useAllMethodsState';
+import allMethodsReducer from '../reducers/allMethods.reducer';
 
 export const AllMethodsContext = createContext();
 
 export function AllMethodsProvider(props) {
-    const postedDataMethods = useAllMethodsState();
+    const [postedImages, dispatch] = useReducer(allMethodsReducer, []);
+    console.log('po', postedImages, dispatch)
+
+    // const postedDataMethods = useAllMethodsState();
 
     return (
-        <AllMethodsContext.Provider value={postedDataMethods}>
+        <AllMethodsContext.Provider value={{ postedImages, dispatch }}>
             {props.children}
         </AllMethodsContext.Provider>
     )
